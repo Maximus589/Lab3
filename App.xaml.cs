a8ee766337736c8da2d57ddae1c6a1831b689bd8
Maximus589 <amriahsaoiitcic@gmail.com>
2025-05-31 09:05:10 +0300

    Добавил behind файлы

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace EconomicStatistics
+{
+    public partial class App : Application
+    {
+        // Можно добавить инициализацию ресурсов при необходимости
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
